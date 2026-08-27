4db66288533725141a404009ccb42c4271f3f583
jahir <espitiasantanajahir@gmail.com>
2026-08-27 13:22:38 -0600

    puse carpetas importantes y un comentario de para que se usarán

---
+++ app/src/main/java/com/pame/carvexa/data/model/Usuario.kt
@@ -0,0 +1,13 @@
+package com.pame.carvexa.data.model
+
+// Aquí se define el modelo (estructura de datos) de un usuario.
+// No guarda usuarios; solo define qué información tendrá cada usuario.
+
+//Ejemplo
+//data class Usuario(
+//    val id: Int,
+//    val nombre: String,
+//    val edad: Int,
+//    val correo: String,
+//    val rol: String
+//)
---
+++ app/src/main/java/com/pame/carvexa/navegation/AppNavegation.kt
@@ -0,0 +1,22 @@
+package com.pame.carvexa.navegation
+
+//Aquí hacemos la relación:
+//@Composable
+//fun AppNavigation() {
+//
+//    val navController = rememberNavController()
+//
+//    NavHost(
+//        navController = navController,
+//        startDestination = Routes.HOME
+//    ) {
+//
+//        composable(Routes.HOME) {
+//            HomeScreen(navController)
+//        }
+//
+//        composable(Routes.DETALLE_AUTO) {
+//            DetalleAutoScreen()
+//        }
+//    }
+//}
